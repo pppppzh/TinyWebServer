2bb8fa126d8af84f4b7f238ec0b6346fb28bd710
pzh <2206898142@qq.com>
2024-03-28 17:11:58 +0800

    添加buff类

---
+++ buffer.h
@@ -0,0 +1,50 @@
+#ifndef BUFFER_h
+#define BUFFER_h
+#include<iostream>
+#include <cstring> 
+#include <unistd.h>  
+#include <sys/uio.h>
+#include <vector> 
+#include <atomic>
+#include <assert.h>
+class buffer
+{
+private:
+    std::vector<char> buffer_;
+    std::atomic<std::size_t> ReadPos_;
+    std::atomic<std::size_t> WritePos_;
+
+    char* Begin();
+    void MakeSpace();
+public:
+    buffer(int size=1024);
+    ~buffer();
+    
+    size_t WritableBytes() const;       
+    size_t ReadableBytes() const ;
+    size_t PrependableBytes() const;
+
+    const char* Peek() const;
+    void EnsureWriteable(size_t len);
+    void HasWritten(size_t len);
+
+    void Retrieve(size_t len);
+    void RetrieveUntil(const char* end);
+
+    void RetrieveAll();
+    std::string RetrieveAllToStr();
+
+    const char* BeginWriteConst() const;
+    char* BeginWrite();
+
+    void Append(const std::string& str);
+    void Append(const char* str, size_t len);
+    void Append(const void* data, size_t len);
+    void Append(const buffer& buff);
+
+    ssize_t ReadFd(int fd, int* Errno);
+    ssize_t WriteFd(int fd, int* Errno);
+    
+};
+
+#endif
