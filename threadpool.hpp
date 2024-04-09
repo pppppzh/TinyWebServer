f4001839ef4d95b31acfec6fb27cee446fbf5f2e
pzh <2206898142@qq.com>
2024-04-09 17:28:02 +0800

    完成threadpool类

--- threadpool.hpp
+++ threadpool.hpp
@@ -11,19 +11,65 @@
 class threadpool
 {
 private:
-    
+    struct pool
+    {
+        std::mutex mtx_;
+        std::condition_variable cond_;
+        bool isClosed;
+        std::queue<std::function<void()>> tasks; 
+    };
+    std::shared_ptr<pool> pool_;
     
 public:
-    threadpool();
+    explicit threadpool(int);
     ~threadpool();
+    template<typename T>
+    void AddTask(T&&);
 };
 
-threadpool::threadpool()
+explicit threadpool::threadpool(int count=8)
+:pool_(std::make_shared<pool>())
 {
+    assert(count>0);
+    for(int i=0;i<count;++i)
+    {
+        std::thread([this](){
+            std::unique_lock<std::mutex> lock(pool_->mtx_);
+            while(!pool_->isClosed)
+            {
+                if(!pool_->tasks.empty())
+                {
+                    auto task = std::move(pool_->tasks.front());
+                    pool_->tasks.pop();
+                    lock.unlock();
+                    task();
+                    lock.lock();
+                }
+                else
+                {
+                    pool_->cond_.wait(lock);
+                }
+            }
+        }).detach();
+    }
 }
 
 threadpool::~threadpool()
 {
+    if(pool_)
+    {
+        std::unique_lock<std::mutex> lock(pool_->mtx_);
+        pool_->isClosed = true;
+    }
+    pool_->cond_.notify_all();
+}
+
+template<typename T>
+void AddTask(T&& task)
+{
+    std::unique_lock<std::mutex> lock(pool_->mtx_);
+    pool_->tasks.emplace(std::forward<T>(task));
+    pool_->cond_.notify_one();
 }
 
 
