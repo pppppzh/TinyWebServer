cc0e01beb9d4fc0df0d7b0f88dd471df54b610a4
pzh <2206898142@qq.com>
2024-04-22 09:39:15 +0800

    add test

--- test.cpp
+++ test.cpp
@@ -1,15 +1,51 @@
 #include "log.h"
+#include "threadpool.hpp"
+#include <features.h>
 
-int main()
-{
-    log *lg = log::Instance();
-    lg->init(0,599);
-    LOG_DEBUG("1111");
-    LOG_DEBUG("11");
-    LOG_INFO("2");
-    LOG_WARN("3");
-    LOG_ERROR("4");
+#if __GLIBC__ == 2 && __GLIBC_MINOR__ < 30
+#include <sys/syscall.h>
+#define gettid() syscall(SYS_gettid)
+#endif
 
+void TestLog() {
+    int cnt = 0, level = 0;
+    log::Instance()->init(level, 0, "./testlog1", ".log");
+    for(level = 3; level >= 0; level--) {
+        log::Instance()->setLevel(level);
+        for(int j = 0; j < 10000; j++ ){
+            for(int i = 0; i < 4; i++) {
+                LOG_BASE(i,"%s 111111111 %d ============= ", "Test", cnt++);
+            }
+        }
+    }
+    cnt = 0;
+    log::Instance()->init(level, 5000,"./testlog2", ".log");
+    for(level = 0; level < 4; level++) {
+        log::Instance()->setLevel(level);
+        for(int j = 0; j < 10000; j++ ){
+            for(int i = 0; i < 4; i++) {
+                LOG_BASE(i,"%s 222222222 %d ============= ", "Test", cnt++);
+            }
+        }
+    }
+}
 
-    return 0;
+void ThreadLogTask(int i, int cnt) {
+    for(int j = 0; j < 10000; j++ ){
+        LOG_BASE(i,"PID:[%04d]======= %05d ========= ", gettid(), cnt++);
+    }
+}
+
+void TestThreadPool() {
+    log::Instance()->init(0,  5000,"./testThreadpool", ".log");
+    threadpool threadpool(6);
+    for(int i = 0; i < 18; i++) {
+        threadpool.AddTask(std::bind(ThreadLogTask, i % 4, i * 10000));
+    }
+    getchar();
+}
+
+int main() {
+    // TestLog();
+    TestThreadPool();
 }
